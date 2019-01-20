85ed336a94bffa3aadc39d49cf482c70b0806b39
attila5287 <42498753+attila5287@users.noreply.github.com>
2019-01-20 08:54:54 -0700

    Add files via upload
    flask module main APP-PY

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
+# import necessary libraries
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_indeed
+from pymongo import MongoClient
+import pymongo
+import JobVizzY
+from JobVizzY import scrapListFrameDict
+from userInput import jobListSampleCut
+from userInput import cityListSampleCut
 
+# create instance of Flask app
 app = Flask(__name__)
-mongo = PyMongo(app, uri="mongodb://localhost:27017/indeed_db")
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/jobViz_app"
+mongo = PyMongo(app)
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.jobViz_db
+# Run scraped functions
+# fulljobVizdata = JobVizzY.scrapListFrameDict(
+#     jobListSampleCut, cityListSampleCut)
+print('')
+# print(len(fulljobVizdata))
+print('apprx 300 jobs test count')
+print('')
+# Insert job listings into mongoDb
+# db.collection.drop()
+# db.collection.insert_many(fulljobVizdata)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def home():
+
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(debug=True)
